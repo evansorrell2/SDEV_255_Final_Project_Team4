2427c373aeda2b0f4b047c432ae272ff03af0483
Connor Comstock <ccomstoc@butler.edu>
2023-07-23 13:57:39 -0400

    working login

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Course = require('./models/course')
+const authRoutes = require('./routes/authRoutes.js');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 // express app
 const app = express();
+
 
 // connect to mongodb
 const dbURI = 'mongodb+srv://connor:1234@nodetuts.oi2jfex.mongodb.net/?retryWrites=true&w=majority';
@@ -19,18 +22,27 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(express.json());
 
-// routes
+//cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//UserInfo
+app.get('*', checkUser);
+
+// -----routes-----
+
+//Auth
+
+app.use(authRoutes);
+
 // index
 app.get('/', (req, res) => {
     res.redirect('/courses');
     //res.render('index', { title: 'Home'});   
 });
 
-// login
-app.get('/login', (req, res) => {
-    res.render('login', { title: 'Login' });
-});
 
 
 // course routes
@@ -59,7 +71,7 @@ app.post('/courses', (req, res) => {
 })
 
 // create course
-app.get('/courses/create', (req, res) => {
+app.get('/courses/create', requireAuth, (req, res) => {
     res.render('create', { title: 'Create Course' });
 });
 
